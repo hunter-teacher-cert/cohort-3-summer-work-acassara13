b0fc7403910a97b801b43895b1298b6363077c8c
AASINE CASSARA <85954161+acassara13@users.noreply.github.com>
2022-07-01 18:00:04 +0000

    2

---
+++ programming/1/Nim.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Nim{
+  public static void main(String[] args)
+  {
+    int stones = 12;
+    int stonesTaken;
+    int stonesLeft;
+
+    Scanner input = new Scanner(System.in);
+
+    //loop until game ends
+    while (condition) {
+      TM_SELECTED_TEXT
+        //prompt user for input
+public int computerTurn(int stonesLeft){
+     int compT = (int) (Math.random()*2)+1;
+      return compT;
+}
+ public void playGame(){
+   Scanner input = new Scanner(System.in);
+  System.out.println
+ } //calculate number of stones taken and print
+
+        //check for win
+
+        //computer turn
+
+        //check win
+       
+  }
+}
