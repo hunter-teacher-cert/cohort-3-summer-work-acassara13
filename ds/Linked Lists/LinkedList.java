a46298d4b1c239649ad4f8aef285b6b966722aa0
AASINE CASSARA <85954161+acassara13@users.noreply.github.com>
2022-07-19 01:43:46 +0000

    LINKED COMPLETED

--- ds/Linked Lists/LinkedList.java
+++ ds/Linked Lists/LinkedList.java
@@ -4,13 +4,11 @@ import java.util.*;
 /**
 For all attempted methods, make sensible decisions for error and
 edge cases (such as indexing out of bounds).
-
 Basic
 -----
 add(string value)
 get(int index);
 toString()
-
 
 Intermediate (at least add, size + one of the other two)
 ------------
@@ -18,7 +16,6 @@ size()
 add(int index,String value)
 indexOf(String value);
 toArray()
-
 
 Challenge
 --------
@@ -36,32 +33,54 @@ public class LinkedList{
   /**
   Parameters:
   value - the new string to add to the list
-
   Adds a new node containing value to the front of the list.
   */
   public void add(String value){
 
+    Node a = new Node(value, head);
+    head = a;
   }
 
   /**
   Returns the String in the node at location index.
   */
   public String get(int index){
-    return "";
+
+    int i=0;
+    Node temp = head;
+    while(i<index) {
+      temp = temp.getNext();
+      i += 1;
+    }
+    
+    return temp.getData();
   }
 
   /**
   Return a string representation of the list
   */
   public String toString(){
-    return "";
+    String list = "";
+    Node temp = head;
+    while(temp != null) {
+      list = list+(temp);
+      temp = temp.getNext();
+    }
+    
+    return list;
   }
 
   /**
   returns the number of elements in the list
   */
   public int size(){
-    return 0;
+    int i=0;
+    Node temp = head;
+    while(temp != null) {
+      temp = temp.getNext();
+      i += 1;
+    }
+    return i;
   }
 
 
@@ -70,62 +89,116 @@ public class LinkedList{
   Parameters:
   index - an int with the location to add
   value - the new value
-
   Adds a new node with the String value to the list.
   The new node should be added at the location specified by the index.
-
   For example, given the list:
   "a" -> "b" -> "c" -> "d"
-
   add(1,"z") results in:
   "a"-> "z" -> "b" -> "c" -> "d"
-
   */
   public void add(int index, String value){
 
+    // Node I = new Node(value);
+    // Node temp=head;
+    // //set temp to word using a while loop
+
+    
+    // temp = I.setNext
+  Node I = new Node(value);
+		//I.setNext() = null;
+		
+		if(index==0)
+		{
+			add(value);
+		}
+		else{
+		Node temp = head;
+		for(int i=0;i<index-1;i++)
+		{
+			temp = temp.getNext();
+		}
+		I.setNext(temp.getNext()); 
+		temp.setNext(I);
+		}
+    
   }
 
 
   /**
   Returns the index (location) of the first node in the list
   that contains value.
-
   Example:
   Given the list:
   "a"->"b"->"c"->"d"->"e"
   indexOf("d") would return 3 since "d" is at location 3.
-
   */
   public int indexOf(String value){
-    return 0;
+    Node temp = head;
+      int i = 0;
+      while (temp !=null){
+    
+        if (temp.getData()==value){
+          return i;
+        }
+        else{
+          i++;
+          temp = temp.getNext();
+        }
+      }
+    return -1;
   }
 
 
   /**
   This routine should create a new array that is the same
   size as the number of Nodes in the list.
-
   It should then copy all of the values to the array and return
   the array.
-
   */
+  //we're going through and creating a for loop and then copying it. Creating a String temp variable copies the array. We used size to call the linked list
   public String[] toArray(){
-    return null;
+    String [] a= new String[size()];
+    Node temp=head;
+    for (int i=0; i<size(); i++){
+     a[i]=temp.getData();
+      temp=temp.getNext();
+    }
+    
+    return a;
   }
 
 
 
   /**
   Remove the Node at location index from the list.
-
   Ex:
   
   Given the list:
   "a"->"b"->"c"->"d"->"e"
-
   remove(2) results in:
   "a"->"b"->"d"->"e"
   */
   public void remove(int index){
+    Node temp = head;
+    Node n1 = null;
+    if(index==0)
+		{
+			head = head.getNext();
+		}
+    else{
+		
+		for(int i=0;i<index-1;i++)
+		{
+			temp = temp.getNext();
+		}
+		n1.setNext(temp.getNext()); 
+		temp.setNext(n1);
+		}
+    
+			n1 = temp.getNext();
+			n1.setNext(n1);
+			//System.out.println("n1 " + n1.data);
+			n1 = null;
+		}
   }
-}
+
